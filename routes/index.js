c952df7d05fa7cad2c0069fab132ae8d9a62fa4c
Daniel Grigo <daniel.grigo@freenet.de>
2020-04-21 08:08:38 +0200

    Addes password hashing

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const {User, Course} = require('../models').models;
-const bcryptjs = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 /* Handler function to wrap each route which does async requests to the db.
 Catches any errors and forwards them to the error handler. With high regards to teamtreehouse.com where the
 idea for this function and in parts the code were part of a lecture.*/
@@ -16,24 +16,20 @@ function asyncHandler(cb){
   }
 }
 
+
 router.get("/users", asyncHandler (async(req, res) =>{
   const users = await User.findAll();
   res.json(users)
 }));
 
 router.post("/users", asyncHandler (async(req, res, next) =>{
-  try {
-    await User.create(req.body);
-    res.status(201).location('/').end();
-  } catch(error){
-      if (error.name === 'SequelizeValidationError'){
-        error.status = 400;
-        next(error)
-        // console.log(error);
-        // const errors= error.errors
-        // res.sendStatus(400).end();
-      }
+    const hash = bcrypt.hashSync(req.body.password);
+    const newUser = {
+      ...req.body,
+      password: hash
     }
+    await User.create(newUser);
+    res.status(201).location('/').end();
 }));
 
 router.get("/courses", asyncHandler (async(req, res) =>{
@@ -46,7 +42,7 @@ router.get("/courses", asyncHandler (async(req, res) =>{
 }));
 
 router.get("/courses/:id", asyncHandler (async(req, res) =>{
-  const courses = await Course.findAll({
+  const course = await Course.findOne({
     where: {
       id: req.params.id
     },
@@ -54,23 +50,24 @@ router.get("/courses/:id", asyncHandler (async(req, res) =>{
       model: User
     }
   });
-  res.json(courses)
+  res.json(course)
 }));
 
 router.post("/courses", asyncHandler (async(req, res, next) =>{
-  try {
     const course = await Course.create(req.body);
-    console.log(course);
     res.status(201).location(`/courses/${course.id}`).end();
-  } catch(error){
-      if (error.name === 'SequelizeValidationError'){
-        error.status = 400;
-        next(error)
-        // console.log(error);
-        // const errors= error.errors
-        // res.sendStatus(400).end();
-      }
-    }
+}));
+
+router.put("/courses/:id", asyncHandler (async(req, res, next) =>{
+    const course = await Course.findByPk(req.params.id);
+    await course.update(req.body);
+    res.status(204).end();
+}));
+
+router.delete("/courses/:id", asyncHandler (async(req, res, next) =>{
+    const course = await Course.findByPk(req.params.id);
+    await course.destroy();
+    res.status(204).end();
 }));
 
 module.exports = router;
