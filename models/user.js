d0c927e6b22b780fa7dc38a578c5961f9f68085b
Daniel Grigo <daniel.grigo@freenet.de>
2020-04-24 07:52:21 +0200

    Added validation & messages

--- models/user.js
+++ models/user.js
@@ -6,19 +6,35 @@ module.exports = (sequelize) => {
   User.init({
     firstName: {
       type: Sequelize.STRING,
-      allowNull: false
+      allowNull: false,
+      validate:{
+        notEmpty: { msg: "Please enter your first name." },
+      }
     },
     lastName: {
       type: Sequelize.STRING,
-      allowNull: false
+      allowNull: false,
+      validate:{
+        notEmpty: { msg: "Please enter your first name." },
+      }
     },
     emailAddress : {
       type: Sequelize.STRING,
-      allowNull: false
+      unique: { msg: "It seems there's already a user registered with this emailadress" },
+      allowNull: false,
+      validate: {
+        is: {
+          args: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+          msg: "This is not a valid emailadress. Please check if you spelled it correctly."
+        }
+      }
     },
     password: {
       type: Sequelize.STRING,
-      allowNull: false
+      allowNull: false,
+      validate:{
+        notEmpty: { msg: "Please enter a password" },
+      }
     }
   }, {sequelize});
 
