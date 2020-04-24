952a6fc11aeb2efc5d56b4911bca2dd4d9547398
Daniel Grigo <daniel.grigo@freenet.de>
2020-04-24 07:51:27 +0200

    Added comments

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ function asyncHandler(cb){
     }
   }
 }
-
+// Middleware for authenticating a user for certain routes, using basic auth
 async function authentication (req, res, next){
     if(auth(req)){
       const userEmail = auth(req).name;
@@ -25,7 +25,7 @@ async function authentication (req, res, next){
             emailAddress: userEmail
           }
         });
-
+//Comparing the hashed password on the request to the one in the database
       if (user && bcrypt.compareSync(auth(req).pass, user.password)){
           console.log("Authentication successful");
           req.user = user;
@@ -41,7 +41,7 @@ async function authentication (req, res, next){
         });
       }
 }
-
+// Returns the authenticated user, omitting sensitive or unneceassary information in the response
 router.get("/users", asyncHandler(authentication), (req, res) =>{
   const authUser = {
     id: req.user.id,
@@ -51,31 +51,17 @@ router.get("/users", asyncHandler(authentication), (req, res) =>{
   }
   res.json(authUser)
 });
-
+//Creates a new user entry in the db
 router.post("/users", asyncHandler (async(req, res, next) =>{
-
-    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    const existingUser = await User.findOne(
-      { where: {
-          emailAddress: req.body.emailAddress
-        }
-      });
-
-    if(!emailRegex.test(req.body.emailAddress)){
-      res.status(400).json({message: "This is not a valid emailadress. Please check if you spelled it correctly."})
-    } else if (existingUser) {
-      res.status(400).json({message: "It seems there's already a user registered with this emailadress"})
-    } else {
-        const hash = bcrypt.hashSync(req.body.password);
-        const newUser = {
-          ...req.body,
-          password: hash
-        }
-        await User.create(newUser);
-        res.status(201).location('/').end();
-    };
+    //validating the user input before saving just in case the password is omitted
+    const newUser = User.build(req.body);
+    await newUser.validate();
+    const hash = bcrypt.hashSync(newUser.password);
+    newUser.password =  hash
+    await newUser.save();
+    res.status(201).location('/').end();
 }));
-
+// Returns a list of courses, omitting sensitive or unneceassary information in the response
 router.get("/courses", asyncHandler (async(req, res) =>{
   const courses = await Course.findAll({
     attributes: { exclude: ['createdAt', 'updatedAt'] },
@@ -86,13 +72,13 @@ router.get("/courses", asyncHandler (async(req, res) =>{
   });
   res.json(courses)
 }));
-
+//Lets the authenticated user create a new course
 router.post("/courses", asyncHandler(authentication), asyncHandler (async(req, res, next) =>{
     req.body.userId = req.user.id;
     const course = await Course.create(req.body);
     res.status(201).location(`/courses/${course.id}`).end();
 }));
-
+// Returns a course with matching id, omitting sensitive or unneceassary information in the response. Throws an error if no course is found.
 router.get("/courses/:id", asyncHandler (async(req, res) =>{
   const course = await Course.findOne({
     where: {
@@ -109,20 +95,36 @@ router.get("/courses/:id", asyncHandler (async(req, res) =>{
   }
   res.json(course)
 }));
-
+/*Lets the authenticated user update the data of a course he owns. Returns a "bad request" if the json send is empty, contains invalid keynames or
+tries to set title and/or description to an empty string
+*/
 router.put("/courses/:id", asyncHandler(authentication), asyncHandler (async(req, res, next) =>{
     const course = await Course.findByPk(req.params.id);
     if(!course){
       throw new Error("No entry found")
     };
     if(course.userId === req.user.id){
+      if(req.body === {}){
+        res.status(400).json({message: "The request contained no data to be updated"})
+      };
+      //this part checks if the keys send by the user are actually matching to the ones on the course instance the user tries to update
+      let notKeys = [];
+      Object.keys(req.body).forEach(key => {
+          if(!Object.keys(course).find(thisKey => thisKey === key)){
+            notKeys.push(key);
+          };
+      });
+      if (notKeys.length > 0){
+        res.status(400).json({message: `The request contained following invalid attributes: ${notKeys.join(", ")}`})
+      }
+      //finally updating if the data is ok
       await course.update(req.body);
       res.status(204).end();
     } else {
       res.status(403).json({message: "You're not the registered owner of this course"})
     }
 }));
-
+//If the user is authenticated, he may delete the matching cours entry
 router.delete("/courses/:id", asyncHandler(authentication), asyncHandler (async(req, res, next) =>{
     const course = await Course.findByPk(req.params.id);
     if(!course){
